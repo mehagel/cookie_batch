a922bebb306099f83fd4c527d940989f04104406
Apprentice <apprentice@devbootcamp.com>
2013-09-04 12:27:30 -0700

    cookie function and views

--- scripts.js
+++ scripts.js
@@ -1,8 +1,5 @@
 var CookieModel = function(data){
-	var count = count || 0;
-	count++;
-
-	this.id = count;
+	this.id = data.id;
 	this.name = data.name;
 	this.bakeTime = data.bakeTime;
 	this.totalTime = 0;
@@ -24,12 +21,65 @@ CookieModel.prototype.status = function(){
 	}
 };
 
-var CookiePrepView = function(model){
-	this.$elem = $("<li></li>");
+var CookieCollection = function(){
+	this.count = 0;
+	this.cookies = [];
 };
 
+CookieCollection.prototype.add = function(data){
+	this.count++;
+	var cookie = new CookieModel({
+		id: this.count,
+		name: data.name,
+		bakeTime: data.bakeTime
+	});
+	this.cookies.push(cookie);
+	return cookie;
+};
+
+var CookiePrepView = function(model){
+	this.model = model;
+	this.$elem = $("<li></li>");
+	this.template = _.template($('#cookie-prep-template').html());
+};
+
+CookiePrepView.prototype.render = function(){
+	this.$elem.html(this.template({model:this.model}));
+	return this.$elem;
+};
+
+CookiePrepView.prototype.createEventHandlers = function(){
+	var that = this;
+	this.$elem.on('click', 'button', function(e){
+			var view = new CookieOvenView(that.model);
+			console.log(view);
+			$('#rack_' + that.model.id.toString()).html(view.render());
+	});
+};
+
+var CookieOvenView = function(model){
+	this.model = model;
+	this.$elem = $("<div></div>");
+	this.template = _.template($('#cookie-oven-template').html());
+};
+
+CookieOvenView.prototype.render = function(){
+	this.$elem.html(this.template({model:this.model}));
+	return this.$elem;
+};
 
 $(document).ready(function(){
-
-
+	cookieCollection = new CookieCollection();
+	$('#new_batch').on('submit', function(e){
+		e.preventDefault();
+		var name = $(this).find('input[name=batch_type]').val();
+		var time = $(this).find('input[name=bake_time]').val();
+		var cookie = cookieCollection.add({
+			name: name,
+			bakeTime: parseInt(time)
+		});
+		var view = new CookiePrepView(cookie);
+		view.createEventHandlers();
+		$('#prep_batches').append(view.render());
+	});
 });
