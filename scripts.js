09eefabe70e193d5ab96f5a31c5b382ef9378b48
Apprentice <apprentice@devbootcamp.com>
2013-09-04 10:40:50 -0700

    cookie objects

--- scripts.js
+++ scripts.js
@@ -1,13 +1,35 @@
+var CookieModel = function(data){
+	var count = count || 0;
+	count++;
+
+	this.id = count;
+	this.name = data.name;
+	this.bakeTime = data.bakeTime;
+	this.totalTime = 0;
+};
+
+CookieModel.prototype.bake = function(){
+	this.totalTime++;
+};
+
+CookieModel.prototype.status = function(){
+	if (this.totalTime === 0) {
+		return "raw";
+	} else if (this.totalTime < this.bakeTime) {
+		return "still_gooey";
+	} else if (this.totalTime == this.bakeTime) {
+		return "just_right";
+	} else {
+		return "crispy";
+	}
+};
+
+var CookiePrepView = function(model){
+	this.$elem = $("<li></li>");
+};
+
+
 $(document).ready(function(){
 
 
-
-
-
-
-
-
-
-
-	
-})
+});
